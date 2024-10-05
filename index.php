1511a633beeaa8738472509fcb4027abff74a7c9
Vladislav <vladislav.vladvlad2015@yandex.ru>
2024-10-05 11:34:10 +0300

    Second commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once ("connect.php");
+/*print_r($connect);*/
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Desription</th>
+            <th>Price</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Product 1</td>
+            <td>Lorem ipsum</td>
+            <td>1000$</td>
+        </tr>
+    </table>
+</body>
+</html>
